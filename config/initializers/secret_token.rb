5617d7a36b39dd98aa18caaa29a7f5da1031edf4
Phillip Landers <planders.tech@gmail.com>
2013-08-19 17:24:12 -0400

    Setup App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuestingCloud::Application.config.secret_key_base = 'b47c5c6cffb52c9912363a61030efe18c27623ea812a6abfa75a8cd5d0a5686c3f5f9234cd18569a1777d266ea3b48b519949d5e7615613a26c89ec8c6c446d7'
